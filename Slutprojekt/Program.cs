d6af2a1754ac0fb062c23b286b559a322833178d
Elliot <walker.elliot@elev.ga.ntig.se>
2023-04-09 12:56:47 +0200

    Put in the simple raylib things

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.ReadLine();
+﻿global using Raylib_cs; 
+global using System.Numerics;
+global using System.Collections.Generic;
+global using RestSharp;
+global using System.Text.Json;
+global using System.Net;
+
+Raylib.SetTargetFPS(60);
+Raylib.InitWindow(800, 600, "Maze Game");
+
+Map maze = new Map();
+
+while (!Raylib.WindowShouldClose()) {
+
+Raylib.BeginDrawing();
+
+Raylib.EndDrawing();
+
+
+}
